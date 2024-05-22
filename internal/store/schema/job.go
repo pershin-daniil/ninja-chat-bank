cc48fd51b0bcda0059663cbd4853a710d363d8cd
Daniil Pershin <pershin.daniil.e@gmail.com>
2024-05-22 15:30:21 +0300

    fix: update lint

--- internal/store/schema/job.go
+++ internal/store/schema/job.go
@@ -1,11 +1,11 @@
 package schema
 
 import (
-	"entgo.io/ent/schema/index"
 	"time"
 
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
 
 	"github.com/pershin-daniil/ninja-chat-bank/internal/types"
 )
